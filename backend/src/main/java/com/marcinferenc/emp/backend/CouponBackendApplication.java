08732672366389bcb53eb15f005ecde7378d09a0
Marcin Ferenc <marcin.ferenc@infor.com>
2026-06-26 12:31:43 +0200

    app main class renamed for easy project navigation

--- backend/src/main/java/com/marcinferenc/emp/backend/CouponBackendApplication.java
+++ backend/src/main/java/com/marcinferenc/emp/backend/CouponBackendApplication.java
@@ -4,10 +4,10 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class CouponApplication {
+public class CouponBackendApplication {
 
 	public static void main(String[] args) {
-		SpringApplication.run(CouponApplication.class, args);
+		SpringApplication.run(CouponBackendApplication.class, args);
 	}
 
 }
