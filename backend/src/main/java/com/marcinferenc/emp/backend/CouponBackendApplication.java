0473eedd737b01aee2f40d78038abd5902c60554
Marcin Ferenc <marcin.ferenc@infor.com>
2026-06-26 19:54:46 +0200

    fixed bug with image pull policy

--- backend/src/main/java/com/marcinferenc/emp/backend/CouponBackendApplication.java
+++ backend/src/main/java/com/marcinferenc/emp/backend/CouponBackendApplication.java
@@ -1,13 +1,16 @@
 package com.marcinferenc.emp.backend;
 
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
+@Slf4j
 public class CouponBackendApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(CouponBackendApplication.class, args);
+		log.info("application started");
 	}
 
 }
