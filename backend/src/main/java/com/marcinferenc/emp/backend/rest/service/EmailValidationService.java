24869a12598e4961e0e59b1d5a9ae2964d8aeee2
Marcin Ferenc <marcin.ferenc@infor.com>
2026-06-26 14:00:22 +0200

    email validation service

---
+++ backend/src/main/java/com/marcinferenc/emp/backend/rest/service/EmailValidationService.java
@@ -0,0 +1,5 @@
+package com.marcinferenc.emp.backend.rest.service;
+
+public interface EmailValidationService {
+    void validateEmailAddress(String emailAddress);
+}
---
+++ backend/src/main/java/com/marcinferenc/emp/backend/rest/service/EmailValidationServiceImpl.java
@@ -0,0 +1,14 @@
+package com.marcinferenc.emp.backend.rest.service;
+
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.stereotype.Service;
+
+@Service
+@Slf4j
+public class EmailValidationServiceImpl implements EmailValidationService {
+
+    @Override
+    public void validateEmailAddress(String emailAddress) {
+
+    }
+}
