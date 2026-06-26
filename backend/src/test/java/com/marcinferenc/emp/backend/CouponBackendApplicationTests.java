1311c4a0fd3cde753e549b45db8e34ccc8edbea8
Marcin Ferenc <marcin.ferenc@infor.com>
2026-06-26 13:53:55 +0200

    contextLoads() fix

--- backend/src/test/java/com/marcinferenc/emp/backend/CouponBackendApplicationTests.java
+++ backend/src/test/java/com/marcinferenc/emp/backend/CouponBackendApplicationTests.java
@@ -3,7 +3,11 @@ package com.marcinferenc.emp.backend;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
-@SpringBootTest
+@SpringBootTest(properties = {
+		"spring.autoconfigure.exclude="
+			+ "org.springframework.boot.jdbc.autoconfigure.DataSourceAutoConfiguration,"
+			+ "org.springframework.boot.hibernate.autoconfigure.HibernateJpaAutoConfiguration"
+})
 class CouponBackendApplicationTests {
 
 	@Test
